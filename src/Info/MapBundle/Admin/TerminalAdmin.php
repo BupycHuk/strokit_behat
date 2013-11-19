8b4ee48b680e5da52cfe9030e411586d3d42289e
Nurlan Moldomurov <bupychuk1989@gmail.com>
2013-11-20 03:41:06 +0600

    ELS-6 ???????? ????? ??/??? ? ???????

---
+++ src/Info/MapBundle/Admin/TerminalAdmin.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Info\MapBundle\Admin;
+
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Show\ShowMapper;
+use Sonata\MediaBundle\Provider\Pool;
+
+class TerminalAdmin extends Admin
+{
+    protected $pool;
+
+    /**
+     * @param string                            $code
+     * @param string                            $class
+     * @param string                            $baseControllerName
+     * @param \Sonata\MediaBundle\Provider\Pool $pool
+     */
+    public function __construct($code, $class, $baseControllerName, Pool $pool)
+    {
+        parent::__construct($code, $class, $baseControllerName);
+
+        $this->pool = $pool;
+    }
+
+    /**
+     * @param ListMapper $listMapper
+     */
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->add('id')
+            ->add('location')
+            ->add('address')
+            ->add('description')
+            ->add('_action', 'actions', array(
+                'actions' => array(
+                    'show' => array(),
+                    'edit' => array(),
+                    'delete' => array(),
+                )
+            ))
+        ;
+    }
+
+    /**
+     * @param FormMapper $formMapper
+     */
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->add('location','mapfield')
+            ->add('address')
+            ->add('description')
+            ->add('image', 'sonata_type_model_list', array('required' => false), array(
+                'link_parameters' => array('context'=>'map')
+            ))
+        ;
+    }
+
+    /**
+     * @param ShowMapper $showMapper
+     */
+    protected function configureShowFields(ShowMapper $showMapper)
+    {
+        $showMapper
+            ->add('id')
+            ->add('location')
+            ->add('address')
+            ->add('description')
+        ;
+    }
+    public function getFormTheme() {
+        return array_merge(parent::getFormTheme(),array('InfoMapBundle:Admin:Map.html.twig'));
+    }
+}
---
+++ src/Info/MapBundle/Controller/TerminalAdminController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Info\MapBundle\Controller;
+
+use Sonata\AdminBundle\Controller\CRUDController;
+
+class TerminalAdminController extends CRUDController
+{
+
+}
