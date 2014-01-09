921ab62392b733c7bb35f3786acc9e4859bcb7fd
Nurlan Moldomurov <bupychuk1989@gmail.com>
2014-01-09 11:24:36 +0600

    ELS-35 Вывод списка новостей

--- src/Application/Sonata/NewsBundle/Controller/PostController.php
+++ src/Application/Sonata/NewsBundle/Controller/PostController.php
@@ -46,4 +46,35 @@ class PostController extends BaseController
 
         return $response;
     }
+    /**
+     * @param array $criteria
+     * @param array $parameters
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function renderArchive(array $criteria = array(), array $parameters = array())
+    {
+        $pager = $this->getPostManager()->getPager(
+            $criteria,
+            $this->getRequest()->get('page', 1),
+            $this->container->getParameter('sonatanews_items_in_list')
+        );
+        $pager->setMaxPageLinks(10);
+
+        $parameters = array_merge(array(
+            'pager' => $pager,
+            'blog'  => $this->get('sonata.news.blog'),
+            'tag'   => false,
+            'route' => $this->getRequest()->get('_route'),
+            'route_parameters' => $this->getRequest()->get('_route_params')
+        ), $parameters);
+
+        $response = $this->render(sprintf('SonataNewsBundle:Post:archive.%s.twig', $this->getRequest()->getRequestFormat()), $parameters);
+
+        if ('rss' === $this->getRequest()->getRequestFormat()) {
+            $response->headers->set('Content-Type', 'application/rss+xml');
+        }
+
+        return $response;
+    }
 }
