04204bd3164bbae0032c77b99a812f7aee664918
Nurlan Moldomurov <bupychuk1989@gmail.com>
2014-01-25 03:48:09 +0600

    ELS-36 Генерация бредкрамбов... На серваке что-то не хочет работать

--- src/Strokit/CoreBundle/Menu/BreadcrumbBuilder.php
+++ src/Strokit/CoreBundle/Menu/BreadcrumbBuilder.php
@@ -49,8 +49,8 @@ class BreadcrumbBuilder
         echo 'test1';
 
         // - [@infofaq_breadcrumb, @infomap_breadcrumb, @infopage_breadcrumb, @sonata.news.breadcrumb, @infofeedback_breadcrumb]
-    var_dump($this->container->get('infofaq_breadcrumb'));
-        if ($this->container->get('infofaq_breadcrumb')->createBreadcrumbMenu($request, $menu))
+        $faqBreadcrumbBuilder = new \Info\FaqBundle\Menu\BreadcrumbBuilder();
+        if ($faqBreadcrumbBuilder->createBreadcrumbMenu($request, $menu))
             return $menu;
         echo 'test2';
         if ($this->container->get('infopage_breadcrumb')->createBreadcrumbMenu($request, $menu))
